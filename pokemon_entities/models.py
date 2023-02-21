2ff7beb37119a636a9e9be480057dc68d5771060
svg83 <svgeneraloff@gmail.com>
2023-02-21 10:36:01 +0400

    создана модель Pokemon

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,3 +1,6 @@
 from django.db import models  # noqa F401
 
 # your models here
+class Pokemon(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
