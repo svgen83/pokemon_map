08c2fa2c81e6b50d908708526d3a864c781ef407
svg83 <svgeneraloff@gmail.com>
2023-02-21 11:40:04 +0400

    add Pokemon.image model field

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,6 +4,7 @@ from django.db import models  # noqa F401
 class Pokemon(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return f'{self.title}'
