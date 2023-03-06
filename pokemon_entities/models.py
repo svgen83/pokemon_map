871dd486941d3b74e2bb5a485aaf8b422b077032
svg83 <svgeneraloff@gmail.com>
2023-03-06 16:22:57 +0400

    добавлено описание полей моделей

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,12 +3,23 @@ from django.db import models  # noqa F401
 # your models here
 class Pokemon(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=200, null=True, blank=True)
-    title_en = models.CharField(max_length=200, null=True, blank=True)
-    title_jap = models.CharField(max_length=200, null=True, blank=True)
-    image = models.ImageField(upload_to='images', null=True, blank=True)
-    description = models.TextField(max_length=2000, blank=True,default='')
+    title = models.CharField(max_length=200,
+                             verbose_name = 'русское название',
+                             null=True, blank=True)
+    title_en = models.CharField(max_length=200,
+                                verbose_name = 'английское название',
+                                null=True, blank=True)
+    title_jap = models.CharField(max_length=200,
+                                 verbose_name = 'японское название',
+                                 null=True, blank=True)
+    image = models.ImageField(upload_to='images',
+                              verbose_name = 'изображение',
+                              null=True, blank=True)
+    description = models.TextField(max_length=2000,
+                                   verbose_name = 'описание',
+                                   blank=True,default='')
     previous_evolution = models.ForeignKey('self', null=True,blank=True,
+                                           verbose_name = 'в кого эволюционирует',
                                            related_name='next_evolutions',
                                            on_delete=models.SET_NULL)
 
@@ -17,13 +28,18 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, null=True)
-    latitude = models.FloatField(null=True, blank=True)
-    longitude = models.FloatField(null=True, blank=True)
-    appeared_at = models.DateTimeField(null=True, blank=True)
-    disappeared_at = models.DateTimeField(null=True, blank=True)
-    level = models.IntegerField(default=0)
-    health = models.IntegerField(default=0)
-    strength = models.IntegerField(default=0)
-    defence = models.IntegerField(default=0)
-    stamina = models.IntegerField(default=0)
+    pokemon = models.ForeignKey(Pokemon,verbose_name = 'покемон',
+                                on_delete=models.CASCADE, null=True)
+    latitude = models.FloatField(null=True, blank=True,
+                                 verbose_name = 'широта')
+    longitude = models.FloatField(null=True, blank=True,
+                                  verbose_name = 'долгота')
+    appeared_at = models.DateTimeField(null=True, blank=True,
+                                      verbose_name = 'время появления')
+    disappeared_at = models.DateTimeField(null=True, blank=True,
+                                          verbose_name = 'время исчезновения')
+    level = models.IntegerField(default=0, verbose_name = 'уровень')
+    health = models.IntegerField(default=0, verbose_name = 'здоровье')
+    strength = models.IntegerField(default=0, verbose_name = 'сила')
+    defence = models.IntegerField(default=0, verbose_name = 'сопротивление')
+    stamina = models.IntegerField(default=0, verbose_name = 'выносливость')
